6de48337b9606908c6960b0b9bee25367c2c2ec6
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-05-15 08:05:22 +0900

    add testing via documentation

--- example.py
+++ example.py
@@ -1,2 +1,23 @@
 def reverse(string):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+    """
+    """
+    This is the "example" module.
+
+    The example module supplies one function, reverse().  For example,
+
+    >>> reverse('awesome!')
+    '!emosewa'
+    """
     return string[::-1]
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
